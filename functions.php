fcd06aa1709d105e240cc67733bb82696fa9746e
Astrid Mathilde Andersen <heia@astridandersen.no>
2020-03-14 18:04:22 +0100

    Added additional theme support for Block Editor

--- functions.php
+++ functions.php
@@ -7,16 +7,15 @@
  */
 
 if ( ! function_exists( 'andersen_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 */
+
 	function andersen_setup() {
-		/*
+
+		/**
 		 * Make theme available for translation.
 		 */
 		load_theme_textdomain( '_andersen', get_template_directory() . '/languages' );
 
-		/*
+		/**
 		 * Let WordPress manage the document title.
 		 * By adding theme support, we declare that this theme does not use a
 		 * hard-coded <title> tag in the document head, and expect WordPress to
@@ -24,7 +23,7 @@ if ( ! function_exists( 'andersen_setup' ) ) :
 		 */
 		add_theme_support( 'title-tag' );
 
-		/*
+		/**
 		 * Backward Compatibility for wp_body_open
 		 */
 		if ( ! function_exists( 'wp_body_open' ) ) {
@@ -33,22 +32,21 @@ if ( ! function_exists( 'andersen_setup' ) ) :
 			}
 		}
 
-		/*
+		/**
 		 * Enable support for Post Thumbnails on posts and pages.
 		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		 */
 		add_theme_support( 'post-thumbnails' );
 
-		/*
+		/**
 		 * Register menu
 		 */
-
 		register_nav_menus( array(
 			'main-menu' => esc_html__( 'Hovedmeny', 'andersen' ),
 			'footer-menu' => esc_html__( 'Footer', 'andersen' ),
 		) );
 
-		/*
+		/**
 		 * Switch default core markup for search form, comment form, and comments
 		 * to output valid HTML5.
 		 */
@@ -63,7 +61,76 @@ if ( ! function_exists( 'andersen_setup' ) ) :
 				'style',
 			)
 		);
+
+		/**
+		 * Block Editor
+		 *
+		 * Add default block styles
+		 * @link https://developer.wordpress.org/block-editor/developers/themes/theme-support/#default-block-styles
+		 */
+		add_theme_support( 'wp-block-styles' );
+
+		/**
+		 * Block Editor
+		 *
+		 * Define block font sizes
+		 * @link https://developer.wordpress.org/block-editor/developers/themes/theme-support/#block-font-sizes
+		 */
+		add_theme_support( 'editor-font-sizes', array(
+			array(
+				'name' => __( 'Small', 'andersen' ),
+				'size' => 16,
+				'slug' => 'small'
+			),
+			array(
+				'name' => __( 'Base', 'andersen' ),
+				'size' => 20,
+				'slug' => 'regular'
+			),
+			array(
+				'name' => __( 'Large', 'andersen' ),
+				'size' => 32,
+				'slug' => 'large'
+			),
+			array(
+				'name' => __( 'Display', 'andersen' ),
+				'size' => 56,
+				'slug' => 'huge'
+			)
+		) );
+
+		/**
+		 * Block Editor
+		 *
+		 * Disable custom colors in block color palettes
+		 * @link https://developer.wordpress.org/block-editor/developers/themes/theme-support/#disabling-custom-colors-in-block-color-palettes
+		 */
+		add_theme_support( 'disable-custom-colors' );
+
+		/**
+		 * Block Editor
+		 *
+		 * Disable custom gradients
+		 * @link https://developer.wordpress.org/block-editor/developers/themes/theme-support/#disabling-custom-gradients
+		 */
+		add_theme_support( 'disable-custom-gradients' );
+
+		/**
+		 * Block Editor
+		 *
+		 * Add support for editor styles
+		 * @link https://developer.wordpress.org/block-editor/developers/themes/theme-support/#editor-styles
+		 */
+		add_theme_support('editor-styles');
+		add_editor_style( get_stylesheet_uri() . 'editor-styles.css' );
+
 	}
+
+	/**
+	 * Remove support for custom font sizes
+	 * @link https://developer.wordpress.org/block-editor/developers/themes/theme-support/#disabling-custom-font-sizes
+	 */
+	add_theme_support('disable-custom-font-sizes');
 
 endif;
 add_action( 'after_setup_theme', 'andersen_setup' );
