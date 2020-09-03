2ab02291f2fbda314cad7abb659165f01e181bfd
Astrid Mathilde Andersen <heia@astridandersen.no>
2020-09-03 13:49:00 +0200

    only display menu link when the menu has content

--- header.php
+++ header.php
@@ -1,41 +1,55 @@
 <?php
 /**
 *
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <main id="main">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- * @package andersen
- *
- */
+* The header for our theme
+*
+* This is the template that displays all of the <head> section and everything up until <main id="main">
+*
+* @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+* @package andersen
+*
+*/
 
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<?php wp_head(); ?>
+<link rel="profile" href="https://gmpg.org/xfn/11">
+<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-	<?php wp_body_open(); ?>
+<?php wp_body_open(); ?>
 
-	<header id="site-header" role="banner">
+<header id="site-header" role="banner">
 
-		<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
 
+<?php
+
+/* Check if there is any projects, before displaying the menu link */
+$projects = get_posts( array(
+	'post_type'   => 'post', 
+	'post_status' => 'publish',
+	'orderby'     => 'date',
+	'order'       => 'DESC',
+	) );
+	
+	if ( !empty($projects) ) {
+		
+		?>
 		<div class="site-menu-toggle">
-			<a href="#" class="toggle-menu" role="button" aria-label ="<?php echo esc_html__( 'Åpne meny', 'andersen' ); ?>"><span role="img" aria-label="Åpne">👈🏻</span> Meny</a>
+		<a onclick="toggleMenu()" href="#" class="toggle-menu" role="button" aria-label ="<?php echo esc_html__( 'Åpne meny', 'andersen' ); ?>"><span role="img" aria-label="Åpne">👈🏻</span> Meny</a>
 		</div> <!-- .site-menu-toggle -->
-
-		<div class="container-menu">
-			<?php get_template_part('template-parts/layout/menu', 'content'); ?>
-		</div> <!-- .container-menu -->
-
-	</header><!-- #site-header -->
-
-	<main id="site-content" role="main">
+		
+		<div id="container-menu">
+		<?php get_template_part('template-parts/layout/menu', 'content'); ?>
+		</div> <!-- #container-menu -->
+		<?php } // $projects ?>
+		
+		</header><!-- #site-header -->
+		
+		<main id="site-content" role="main">
--- template-parts/layout/menu-content.php
+++ template-parts/layout/menu-content.php
@@ -1,40 +1,60 @@
 <?php
-
 /**
- * Layout => Menu Content
- */
-
+* Layout => Menu Content
+*/
 ?>
 
-<section class="project-navigation">
-	<h2><?php esc_html__( 'Aktuelle prosjekter', 'andersen' ); ?></h2>
+<div class="site-menu-toggle">
+<a onclick="toggleMenu()" href="#" class="toggle-menu" role="button" aria-label ="<?php echo esc_html__( 'Lukk meny', 'andersen' ); ?>">Lukk <span role="img" aria-hidden="true">👉🏻</span></a>
+</div> <!-- .site-menu-toggle -->
 
-	<nav id="project-links">
-		<ul>
-			<li><a href="#">Svalbard Bryggeri</a></li>
-			<li><a href="#">Lo:Le Landskap og Plan</a></li>
-			<li><a href="#">Røst Kommunikasjon</a></li>
-		</ul>
-	</nav>
+<section class="project-navigation">
+<h2><?php echo esc_html__( 'Aktuelle prosjekter', 'andersen' ); ?></h2>
+
+<nav id="project-navigation" class="project-navigation">
+<ul>
+<?php
+
+$projects = new WP_Query( array(
+	'post_type'      => 'post', 
+	'post_status'    => 'publish',
+	'orderby'        => 'date',
+	'order'          => 'DESC',
+	'posts_per_page' => -1,
+));
+
+while ( $projects->have_posts() ) {
+	$projects->the_post();
+	the_title( '<li><a href="' . get_the_permalink() . '">', '</a></li>' );
+}
+
+wp_reset_postdata();
+
+?>
+</ul>
+</nav>
+
 </section> <!-- .project-navigation -->
 
 <section class="contact-information">
-	<h2 class="screen-reader-text"><?php esc_html__( 'Kontaktinformasjon', 'andersen' ); ?></h2>
-	<?php
+<h2 class="screen-reader-text"><?php echo esc_html__( 'Kontaktinformasjon', 'andersen' ); ?></h2>
+<?php
 
-	$email        = get_field('site_contact');
+$email  = get_field('site_contact');
 
+if ( !empty($email) ) { 
+	
 	?>
+	<p>
+	<span class="screen-reader-text"><?php echo esc_html__( 'E-post:', 'andersen' ); ?></span> <a href="<?php echo $mail['url']; ?>"><?php echo $mail['title']; ?></a>
+	</p>
+	<?php 
+	
+} 
 
-	<?php if ( !empty($email) ) { ?>
-		<p><span class="screen-reader-text"><?php esc_html__( 'E-post:', 'andersen' ); ?></span> <a href="<?php echo $mail['url']; ?>"><?php echo $mail['title']; ?></a></p>
-	<?php } ?>
+if ( have_rows('site_social', 'option') ) {
+	get_template_part( 'template-parts/components/social-profiles' );
+}
 
-	<?php
-
-	if ( have_rows('site_social', 'option') ) {
-		get_template_part( 'template-parts/components/social-profiles' );
-	}
-
-	?>
-	</section> <!-- .contact-information -->
+?>
+</section> <!-- .contact-information -->
