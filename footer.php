ef20244ba26a48e3da209142496b8c7d9b385c1d
Astrid Mathilde Andersen <heia@astridandersen.no>
2020-12-07 16:18:18 +0100

    finally happy with the footer 😌

--- footer.php
+++ footer.php
@@ -1,59 +1,64 @@
 <?php
 /**
 *
- * The template for displaying the footer
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- * @package andersen
- *
- */
+* The template for displaying the footer
+*
+* @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+* @package andersen
+*
+*/
 
 ?>
 
 </main><!-- #main -->
 
 <footer id="site-footer" role="contentinfo">
-	<?php
 
-	$email = get_field('site_contact', 'option');
+<?php 
 
-	if ( !empty($email) ) {
+$email = get_field('site_contact', 'option');
+
+if ( !empty($email) ) { 
+	
+	?>
+	<h2 class="screen-reader-text"><?php echo esc_html__( 'Bunnen av siden', 'andersen' ); ?></h2>
+	
+	<h3 class="footer-heading"><?php echo esc_html__( 'Kontakt', 'andersen' ); ?></h3>
+	<span class="screen-reader-text"><?php echo esc_html__( 'E-post:', 'andersen' ); ?></span> <a href="<?php echo $email['url']; ?>"><?php echo $email['title']; ?></a>
+	<?php } ?>
+	
+	<?php if ( have_rows('site_social', 'option') ) { ?>
+		<h3 class="footer-heading"><?php echo esc_html__( 'Profiler', 'andersen' ); ?></h3>
+		<?php get_template_part( 'template-parts/components/social-profiles' ); ?>
+		<?php } ?>
+
+		<h3 class="footer-heading"><?php echo esc_html__( 'Meny', 'andersen' ); ?></h3>
+		<?php
+
+		wp_nav_menu( array(
+			'menu' => 'footer-menu',
+			'theme_location' => 'footer-menu',
+			'container' => 'nav',
+			'container_id' => 'footer-menu',
+		) );
 
 		?>
-		<h6 class="footer-heading"><?php echo esc_html__( 'Kontakt', 'andersen' ); ?></h6>
-		<span class="screen-reader-text"><?php echo esc_html__( 'E-post:', 'andersen' ); ?></span> <a href="<?php echo $email['url']; ?>"><?php echo $email['title']; ?></a>
-	<?php } ?>
-
-	<?php if ( have_rows('site_social', 'option') ) { ?>
-		<h6 class="footer-heading"><?php echo esc_html__( 'Profiler', 'andersen' ); ?></h6>
+		
+		<div id="footer-byline">
 		<?php
-
-		get_template_part( 'template-parts/components/social-profiles' );
-
-	}
-
-	?>
-
-	<div id="footer-byline">
-		<?php
-
-		$privacy = get_field('site_privacy', 'option');
+		
 		$credits = get_field('site_credits', 'option');
-
-		if ( !empty($privacy) ) {
-			echo '<p>' . $privacy . '</p>';
-		}
-
+		
 		if ( !empty($credits) ) {
 			echo '<p>' . $credits . '</p>';
 		}
-
+		
 		?>
-	</div> <!-- #footer-byline -->
-
-</footer><!-- #site-footer -->
-
-<?php wp_footer(); ?>
-
-</body>
-</html>
+		</div> <!-- #footer-byline -->
+		
+		</footer><!-- #site-footer -->
+		
+		<?php wp_footer(); ?>
+		
+		</body>
+		</html>
