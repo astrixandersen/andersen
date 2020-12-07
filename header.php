f2fc10af967b732b3d37cb893aaa2910756c2f13
Astrid Mathilde Andersen <heia@astridandersen.no>
2020-12-07 16:22:00 +0100

    playing around with a secondary font 🤔

--- header.php
+++ header.php
@@ -19,6 +19,7 @@
 
 <link rel="profile" href="https://gmpg.org/xfn/11">
 <?php wp_head(); ?>
+<link rel="stylesheet" href="https://use.typekit.net/saw7ckg.css">
 
 <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦄</text></svg>">
 </head>
