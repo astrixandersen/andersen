40a93d0488b2c89802933c2efed34dc1c553b470
Astrid Mathilde Andersen <heia@astridandersen.no>
2020-12-08 13:31:43 +0100

    added awesome 404-page 🌊💨

--- index.php
+++ index.php
@@ -1,53 +1,37 @@
 <?php
 /**
- *
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- * @package andersen
- *
- */
+*
+* The main template file
+*
+* This is the most generic template file in a WordPress theme
+* and one of the two required files for a theme (the other being style.css).
+* It is used to display a page when nothing more specific matches a query.
+* E.g., it puts together the home page when no home.php file exists.
+*
+* @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+* @package andersen
+*
+*/
 
 get_header();
 
-if ( have_posts() ) :
-
-	?>
-
-	<header class="page-header">
-		<?php if ( is_home() && ! is_front_page() ) : ?>
-		<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-
-		<?php
-
-		elseif ( is_archive() ) :
-			the_archive_title( '<h1 class="page-title">', '</h1>' );
-			the_archive_description( '<div class="archive-description">', '</div>' );
-		endif;
-
-		?>
-	</header>
-
-	<?php
-
 	while ( have_posts() ) {
 		the_post();
-
-		get_template_part( 'template-parts/content', get_post_type() );
+		
+		?>
+		<article id="page-<?php the_ID(); ?>" <?php post_class(); ?>>
+		
+		<header class="page-header">
+		<?php the_title( '<h2 class="page-title">', '</h2>' ); ?>
+		</header>
+		
+		<div class="page-content">
+		<?php the_content(); ?>
+		</div> <!-- .post-content -->
+		
+		</article>
+		<?php 
+		
 	}
-
-	the_posts_navigation();
-
-	?>
-
-	<?php else : ?>
-		<?php get_template_part( 'template-parts/content', 'none' ); ?>
-	<?php endif; ?>
-
-	<?php
-	get_footer();
+	
+get_footer();
--- singular.php
+++ singular.php
@@ -1,33 +1,33 @@
 <?php
 /**
- * The template for displaying single posts and pages.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- * @package andersen
- *
- */
+* The template for displaying single posts and pages.
+*
+* @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+* @package andersen
+*
+*/
 
 get_header();
 
-while ( have_posts() ) {
-	the_post();
-
-	?>
-
-	<article id="page-<?php the_ID(); ?>" <?php post_class(); ?>>
-
+	while ( have_posts() ) {
+		the_post();
+		
+		?>
+		
+		<article id="page-<?php the_ID(); ?>" <?php post_class(); ?>>
+		
 		<header class="page-header">
-			<?php the_title( '<h2 class="page-title">', '</h2>' ); ?>
+		<?php the_title( '<h2 class="page-title">', '</h2>' ); ?>
 		</header>
-
+		
 		<div class="page-content">
-			<?php the_content(); ?>
+		<?php the_content(); ?>
 		</div> <!-- .post-content -->
-
-	</article>
-
-	<?php
-
+		
+		</article>
+		
+		<?php
+		
 	}
-
-	get_footer();
+	
+get_footer();
