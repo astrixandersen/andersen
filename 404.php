3647cbc8f18c4225454d36bda31bc64577a5bcea
Astrid Mathilde Andersen <heia@astridandersen.no>
2020-12-08 13:33:29 +0100

    added awesome 404-page 🌊

--- 404.php
+++ 404.php
@@ -1,27 +1,32 @@
 <?php
 /**
- *
- * The template for displaying 404-pages (not found)
- *
- * @link https://codex.wordpress.org/Creating_an_Error_404_Page
- * @package andersen
- *
- */
+*
+* The template for displaying 404-pages (not found)
+*
+* @link https://codex.wordpress.org/Creating_an_Error_404_Page
+* @package andersen
+*
+*/
 
 get_header();
 
 ?>
 <section class="error-404 not-found">
 
-	<header class="page-header">
-		<h1 class="page-title"><?php esc_html_e( 'Oida! Siden ble ikke funnet', 'andersen' ); ?></h1>
-	</header><!-- .page-header -->
+<header class="page-header">
+<h2 class="page-title"><?php echo esc_html__( '"Du hører bølgene slå mot berget og vinden suse deg i ørene. Når du snur deg, er du omringet av hav på alle kanter."', 'andersen' ); ?></h2>
+</header>
 
-	<div class="page-content">
-		<p><?php esc_html_e( 'Vi finner ikke det du leter etter. Kanskje det hjelper å gjøre et søk?', 'andersen' ); ?></p>
+<figure>
+<img src="<?php echo get_template_directory_uri(); ?>/assets/images/404-image.jpg">
+<figcaption><?php echo esc_html__( 'Foto: Drew Beamer, unsplash.com', 'andersen' ); ?></figcaption>
+</figure>
 
-		<?php get_search_form(); ?>
-	</div><!-- .page-content -->
+<div class="page-content">
+<p><?php echo esc_html__( 'Det var kanskje ikke meningen at du skulle havne her?', 'andersen' ); ?></p>
+<p><a href="<?php echo home_url( '/' ); ?>">Tilbake på land -></a></p>
+</div> <!-- .post-content -->
+
 
 </section><!-- .error-404 -->
 
