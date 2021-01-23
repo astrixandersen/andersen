14b3338bffdc3e97a2a03a6a07f725c803a27b90
Astrid Mathilde Andersen <heia@astridandersen.no>
2021-01-24 00:25:15 +0100

    done with the menu animations and it's awesome 🥳

--- assets/js/open-menu.js
+++ assets/js/open-menu.js
@@ -10,10 +10,8 @@ function toggleMenu() {
   if (document.body.classList.contains("menu-open")) {
     document.body.classList.add("menu-close");
     document.body.classList.remove("menu-open");
-    console.log("CLOSE IT");
   } else {
     document.body.classList.add("menu-open");
     document.body.classList.remove("menu-close");
-    console.log("carefully open it");
   }
 }
