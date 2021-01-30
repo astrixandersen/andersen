65cbaf25ed405f45e2e3bb5c60ca05b18e5eca53
Astrid Mathilde Andersen <heia@astridandersen.no>
2021-01-30 23:57:54 +0100

    changed back to first version of project hero 😅

--- blocks/project-info/build/index.asset.php
+++ blocks/project-info/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => 'a27e83f9e95d1c8e4b449a2aceb41b90');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => 'b3f082ae3f2eb38baae4693ba04c7645');
